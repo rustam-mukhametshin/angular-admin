64e960e0e8f21689270a349f2b1a0663e2ce9fdb
iproman <itechman@yandex.ru>
2020-04-14 12:15:41 +0500

    Initial commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
