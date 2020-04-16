eb5c89e823971332a6ff3b29891685de8b624ebf
iproman <itechman@yandex.ru>
2020-04-16 11:45:00 +0500

    Generate admin module.

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AdminModule {
+
+}
