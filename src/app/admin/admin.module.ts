b5827e59886a983125727f6fb7cd787a08eb3a97
iproman <itechman@yandex.ru>
2020-04-20 16:46:54 +0500

    Generate base CRUD components.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,17 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 import {LoginPageComponent} from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
     LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
   ],
   imports: [
     CommonModule,
@@ -16,6 +22,9 @@ import {LoginPageComponent} from './login-page/login-page.component';
         path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
           {path: 'login', component: LoginPageComponent},
+          {path: 'dashboard', component: DashboardPageComponent},
+          {path: 'create', component: CreatePageComponent},
+          {path: 'edit', component: EditPageComponent},
         ]
       }
     ])
