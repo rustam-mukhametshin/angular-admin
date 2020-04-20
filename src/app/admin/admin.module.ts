6dbae8c9e01f79ae30ceefb0cb2a20c2b243fdd0
iproman <itechman@yandex.ru>
2020-04-20 16:43:20 +0500

    Generate admin layout.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 
 @NgModule({
+  declarations: [
+    AdminLayoutComponent,
+  ],
   imports: [
     CommonModule,
-    RouterModule.forChild([])
+    RouterModule.forChild([
+      {
+        path: '', component: AdminLayoutComponent, children: [
+
+        ]
+      }
+    ])
   ],
   exports: [
     RouterModule
