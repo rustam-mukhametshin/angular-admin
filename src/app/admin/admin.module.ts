dc3e446e75d5487c0308e8952c8d67b4af2b041d
iproman <itechman@yandex.ru>
2020-04-20 16:45:24 +0500

    By default admin redirect to login page.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,17 +2,20 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
+    LoginPageComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
-
+          {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+          {path: 'login', component: LoginPageComponent},
         ]
       }
     ])
