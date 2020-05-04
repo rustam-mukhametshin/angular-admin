66f6b9f2800551f2e95d9ebe845063cd498787d9
iproman <itechman@yandex.ru>
2020-05-04 07:40:34 +0500

    Login, logout methods.

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,10 +1,27 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FirebaseAuthResponse, User} from '../../../shared/interfaces';
+import {Observable} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+import {tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {
+  }
+
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true;
+
+    return this.http.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,
+      user
+    ).pipe(tap(this.setToken))
+      ;
+  }
+
+  logout() {
+    this.setToken(null);
   }
 
   private setToken(response: FirebaseAuthResponse | null) {
