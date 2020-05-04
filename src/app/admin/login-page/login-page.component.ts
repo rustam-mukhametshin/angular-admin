e27dfd2c14f86f02c416dff09e1e719679421410
iproman <itechman@yandex.ru>
2020-05-04 07:44:01 +0500

    After login to admin panel, navigate to dashboard.

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -10,7 +13,10 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
@@ -30,6 +36,15 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+    });
 
   }
 }
