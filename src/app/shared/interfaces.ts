a0d45e2209ec0a0cb9d34df2987b76dc134e5064
iproman <itechman@yandex.ru>
2020-05-04 07:42:39 +0500

    FirebaseAuthResponse interface.

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,4 +1,16 @@
+/**
+ * User
+ */
 export interface User {
   email: string;
   password: string;
+  returnSecureToken?: boolean;
+}
+
+/**
+ * FirebaseAuthResponse
+ */
+export interface FirebaseAuthResponse {
+  idToken: string;
+  expiresIn: string;
 }
