9d6aef9024910284caa37e95e8cda9660cf4fc90
iproman <itechman@yandex.ru>
2020-05-05 16:53:21 +0500

    Add form submitted status, to disable btn.

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -12,6 +12,7 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
 
   constructor(
     private auth: AuthService,
@@ -36,6 +37,9 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -44,6 +48,9 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
     });
 
   }
