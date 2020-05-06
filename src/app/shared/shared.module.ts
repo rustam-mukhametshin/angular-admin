3b5fe4c66b866dd97902b89ef123402919ef2b3e
iproman <itechman@yandex.ru>
2020-05-06 07:36:56 +0500

    Show quill editor.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {QuillModule} from 'ngx-quill-v2';
 
 @NgModule({
   imports: [
     HttpClientModule,
+    QuillModule
   ],
   exports: [
     HttpClientModule,
+    QuillModule
   ]
 })
 
