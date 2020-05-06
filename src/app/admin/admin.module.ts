ad9e072e867f07842b334324faa069806567483a
iproman <itechman@yandex.ru>
2020-05-06 06:54:47 +0500

    Add guard for routes.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -11,6 +11,7 @@ import {EditPageComponent} from './edit-page/edit-page.component';
 
 import {AuthService} from './shared/services/auth.service';
 import {SharedModule} from '../shared/shared.module';
+import {AuthGuard} from './shared/services/auth.guard';
 
 @NgModule({
   declarations: [
@@ -30,9 +31,9 @@ import {SharedModule} from '../shared/shared.module';
         path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
           {path: 'login', component: LoginPageComponent},
-          {path: 'dashboard', component: DashboardPageComponent},
-          {path: 'create', component: CreatePageComponent},
-          {path: 'post/:id/edit', component: EditPageComponent},
+          {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+          {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
+          {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]},
         ]
       }
     ])
@@ -42,6 +43,7 @@ import {SharedModule} from '../shared/shared.module';
   ],
   providers: [
     AuthService,
+    AuthGuard
   ]
 })
 export class AdminModule {
