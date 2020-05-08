35a1a32716a33360d432f871fe718a48d13aeb86
iproman <itechman@yandex.ru>
2020-05-08 10:48:12 +0500

    Environment configs.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: '',
+  firebaseDbUrl: ''
 };
