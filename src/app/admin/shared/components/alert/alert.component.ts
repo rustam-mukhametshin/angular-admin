a36801a18512d884593c40b0309505f06a024564
iproman <itechman@yandex.ru>
2020-05-12 05:25:53 +0500

    Alert service to alert component.

--- src/app/admin/shared/components/alert/alert.component.ts
+++ src/app/admin/shared/components/alert/alert.component.ts
@@ -1,22 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AlertService} from '../../services/alert.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, OnDestroy {
 
   @Input() delay = 5000;
 
   public text: string;
   public type = 'success';
 
+  private aSub: Subscription;
 
-  constructor() {
+
+  constructor(
+    private alertService: AlertService
+  ) {
   }
 
   ngOnInit() {
+    this.aSub = this.alertService.alert$.subscribe(alert => {
+      this.text = alert.text;
+      this.type = alert.type;
 
+      const timeOut = setTimeout(() => {
+        clearTimeout(timeOut);
+        this.text = '';
+      }, this.delay);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
   }
 }
